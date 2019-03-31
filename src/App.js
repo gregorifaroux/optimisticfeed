9d463e4bb24146fcc1ad31d7639cee2a7ea57d29
Gregori Faroux <gregori.faroux@catalyticds.com>
2019-03-30 23:07:43 -0400

    added title and deploy to firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Optimistic Feed</p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://twitter.com/GregoriFaroux"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            By Gregori
           </a>
         </header>
       </div>
